cd4e9fc5722e15e11ef0d8aa9b29be18f9f787d2
bpandof <bpandofdesarrolador@gmail.com>
2024-07-10 20:53:30 +0200

    primer comentario

---
+++ script_temporal.py
@@ -0,0 +1,4 @@
+mi_variable='hola como estas, mi nombre es bernabé'
+relleno = len('Empezando a sacar el informe_')
+
+print (f"{'#'*(len(mi_variable)+8)}\n### {mi_variable} ###\n{'#'*(len(mi_variable)+8)}")
